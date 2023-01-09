28d1cb93673068f70656dc67c6710258ffd8ab86
fredS86 <fredS86@free.fr>
2023-01-09 11:23:00 +0100

    creation d'un enpoint /api pour les api

--- src/server.js
+++ src/server.js
@@ -1,5 +1,6 @@
 var logger = require('./utils/logger');
 var express = require('express');
+const router = express.Router;
 var serveStatic = require('serve-static');
 var plugs = require('./routers/plugs-server');
 var conf = require('./routers/conf-server');
@@ -12,9 +13,10 @@ var port = process.argv[2] || '8090';
 app.use(serveStatic(__dirname + '/public'));
 logger.debug(__dirname + '/public')
 
-app.use('/plugs', plugs);
-
-app.use('/conf', conf);
+let apiRouter = router()
+    .use('/plugs', plugs)
+    .use('/conf', conf);
+app.use('/api', apiRouter);
 
 logger.debug('LogDir : ' + logger.logDir);
 app.listen(port, function () {
